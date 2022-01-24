9f5ca59b686f8acfb5c265eb20bec9e12dff1837
Amrhub <amr_ahmed65@hotmail.com>
2022-01-25 01:44:19 +0200

    add: Student class

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
+require_relative 'student'
 
 person = Person.new(23, 'Amr', false)
 puts person.can_use_services?
 
-require 'student'
+student = Student.new(5, 23, 'Samaa', false)
+puts student.play_hooky
